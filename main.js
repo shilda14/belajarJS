7364aea6b189180f7be731fa5794ad5850ee4f5d
shilda14 <144862353+shilda14@users.noreply.github.com>
2023-10-12 17:32:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+// console.log("hai")
+// alert("ada notifikasi")
+// prompt("pinjam seratus")
+
+// var promnet ="shilda cantik"
+// console.log(promnet)
+// var promnet ="shilda cantik"
+// console.log(promnet)
+// var promnet ="nothing is posible"
+// console.log(promnet)
+
+// let promnet ="coding is boring"
+// console.log(promnet)
+// let promnet ="coding is boring"
+// console.log(promnet)
+
+// const promnet ="coding is not fun"
+// console.log(promnet)
+// const promnet ="coding is not fun"
+// console.log(promnet)
+
+// let totalPoin = prompt("masukkan Poin anda")
+//     if(totalPoin > 100) {
+//         document.write("selamat");
+//     }else{
+//         document.write("coba lagi");
+//     }
+
+// let x = 6;
+// let y = 3;
+
+// console.log(x < 10 && y > 1); 
+// console.log(x < 10 && y < 1);
+// console.log(x ==5||y==5);
+// console.log(x==6||y==5); 
+// console.log(!(x==y)); 
+
+// document.write(x < 10 && y > 1);
+// document.write("<br>");
+// document.write(x < 10 && y < 1);
+// document.write("<br>");
+// document.write(x ==5||y==5);
+// document.write("<br>");
+// document.write(x==6||y==5);
+// document.write("<br>");
+// document.write(!(x==5));
+
+let p = document.querySelector("P")
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+button.addEventListener('click', function() {
+    let isi = input.value
+    console.log(isi)
+    p.innerHTML = isi
+})
+
+
+
+
+
+
+
+
+
+
+
+
